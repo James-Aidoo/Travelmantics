d1f7c032383ffe29a0303a70fc645078a0735cc0
James Aidoo <james.aidoo@nfortics.com>
2019-08-05 11:33:04 +0000

    Removed Unused imports

--- app/src/main/java/com/questdev/travelmantics/utils/FirebaseUtil.kt
+++ app/src/main/java/com/questdev/travelmantics/utils/FirebaseUtil.kt
@@ -1,11 +1,7 @@
 package com.questdev.travelmantics.utils
 
-import android.app.Activity
-import android.content.Intent
-import android.util.Log
 import android.widget.Toast
 import com.firebase.ui.auth.AuthUI
-import com.firebase.ui.auth.IdpResponse
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseAuth.AuthStateListener
 import com.google.firebase.database.*
